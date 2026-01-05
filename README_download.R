ede5d784a874c1ed4d2fe362f38abb8e6750d93a
Brian Z <brianfzk2025@proton.me>
2026-01-04 21:26:45 -0600

    Fix formatting issues in README_download.R

--- README_download.R
+++ README_download.R
@@ -7,9 +7,9 @@ for (pkg in pkg_need) {
 }
 
 # Install my package using remotes
-devtools::install_github("BrianIZKom1911/CTRF@v0.1.0") 
+remotes::install_github("BrianIZKom1911/CTRF@v0.1.0") 
 # ... or devtools if you really want
-#remotes::install_github("BrianIZKom1911/CTRF@v0.1.0")
+#devtools::install_github("BrianIZKom1911/CTRF@v0.1.0")
 
 # Then simply call 
 library(CTRF)
